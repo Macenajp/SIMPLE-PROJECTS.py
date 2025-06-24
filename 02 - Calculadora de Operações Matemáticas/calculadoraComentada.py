0cd33206c1c8485d37c2a2fc9b8484523fe8ef2b
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-24 16:51:13 -0300

    Update calculadoraComentada.py

--- 02 - Calculadora de Operações Matemáticas/calculadoraComentada.py
+++ 02 - Calculadora de Operações Matemáticas/calculadoraComentada.py
@@ -3,7 +3,7 @@ from time import sleep
 
 # Sessão onde o usuário vai escolher o tipo de operação matemática (ou sair da execução)
 def seleçãoDeOperação():
-    print("Qual o tipo de operação que você quer fazer. \n0 -> Sair \n1 -> Soma \n2 -> Subtração \n3 -> Divisão comum\n4 -> Multiplicação \n5 -> Divisão inteira \n6 -> Resto de uma divisão")
+    print("Qual o tipo de operação que você quer fazer. \n0 -> Sair \n \n1 -> Soma",(" ") * 12, "5 -> Divisão inteira \n2 -> Subtração", (" ") * 7, "6 -> Resto de uma divisão \n3 -> Divisão comum", (" ") * 3, "7 -> Exponenciação \n4 -> Multiplicação")
     return int(input("\nEscolha: "))
 
 # Função para somar os dois números escolhidos
@@ -29,6 +29,9 @@ def divInteira(numero1, numero2):
 # Função que calcula o resto da divisão dos números escolhidos
 def restoDaDivisão(numero1, numero2):
    return numero1 % numero2
+
+def exponenciação(numero1, numero2):
+   return numero1 ** numero2
 
 # Sessão principal, onde tudo o que aparece para o usuário passa por aqui.
 def main():
@@ -115,6 +118,15 @@ def main():
                 print("====================================\n")
                 continue
 
+            elif escolha == 7:
+                print("Muito bem, você quer saber o resultado de uma exponenciação. Quais números você quer usar no cálculo?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                sleep(1)
+                print(f"\nO resultado é: {exponenciação(numero1, numero2):.2f}")
+                print("====================================\n")
+                continue
+
 
             else:
                 print("\nEscolha uma das opções oferecidas!")
@@ -122,5 +134,5 @@ def main():
 
             break
         except ValueError:
-            print("Escolha um valor válido (entre 0 e 6)!")
+            print("Escolha um valor válido (entre 0 e 7)!")
 main()
