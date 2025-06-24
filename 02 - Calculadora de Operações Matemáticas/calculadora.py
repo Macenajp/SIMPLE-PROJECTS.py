7788b9297ffe5f5f04bd78c5e2b4bb191de19aed
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-24 02:22:53 -0300

    Create calculadora.py

---
+++ 02 - Calculadora de Operações Matemáticas/calculadora.py
@@ -0,0 +1,106 @@
+def seleçãoDeOperação():
+    print("Qual o tipo de operação que você quer fazer. \n0 -> Sair \n1 -> Soma \n2 -> Subtração \n3 -> Divisão comum\n4 -> Multiplicação \n5 -> Divisão inteira \n6 -> Resto de uma divisão")
+    return int(input("\nEscolha: "))
+
+def soma(numero1, numero2):
+    return numero1 + numero2
+
+def subtração(numero1, numero2):
+    return numero1 - numero2
+
+def divisão(numero1, numero2):
+    return numero1 / numero2
+
+def multiplicação(numero1, numero2):
+    return numero1 * numero2
+
+def divInteira(numero1, numero2):
+    return numero1 // numero2
+
+def restoDaDivisão(numero1, numero2):
+   return numero1 % numero2
+
+def main():
+    while True:
+        try:
+            escolha = seleçãoDeOperação()
+
+            if escolha == 0:
+                print("Obrigado por testar essa calculadora, volte sempre!")
+                break
+
+          
+            if escolha == 1:
+                print("Muito bem, você escolheu 'soma'. Quais números você quer somar?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                print(f"\nO resultado é: {soma(numero1, numero2):.2f}")
+                print("============================================\n")
+                continue
+
+          
+            elif escolha == 2:
+                print("Muito bem, você escolheu 'subtração'. Quais números você quer usar no cálculo?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                print(f"\nO resultado é: {subtração(numero1, numero2):.2f}")
+                print("====================================\n")
+                continue
+
+
+            elif escolha == 3:
+                print("Muito bem, você escolheu 'divisão comum'. Quais números você quer usar no cálculo?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                if numero2 == 0:
+                    print("\nImpossível com esse número.\n")
+                    print("====================================\n")
+                    continue
+                print(f"\nO resultado é: {divisão(numero1, numero2):.2f}")
+                print("====================================\n")
+                continue
+
+          
+            elif escolha == 4:
+                print("Muito bem, você escolheu 'multiplicação'. Quais números você quer usar no cálculo?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                print(f"\nO resultado é: {multiplicação(numero1, numero2):.2f}")
+                print("====================================\n")
+                continue
+
+        
+            elif escolha == 5:
+                print("Muito bem, você escolheu 'divisão inteira'. Quais números você quer usar no cálculo?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                if numero2 == 0:
+                    print("\nImpossível com esse número.\n")
+                    print("====================================\n")
+                    continue
+                print(f"\nO resultado é: {divInteira(numero1, numero2):.2f}")
+                print("====================================\n")
+                continue
+
+          
+            elif escolha == 6:
+                print("Muito bem, você quer saber o 'resto de uma divisão'. Quais números você quer usar no cálculo?")
+                numero1 = int(input("Primeiro número: "))
+                numero2 = int(input("Segundo número: "))
+                if numero2 == 0:
+                    print("\nImpossível com esse número.\n")
+                    print("====================================\n")
+                    continue
+                print(f"\nO resultado é: {restoDaDivisão(numero1, numero2):.2f}")
+                print("====================================\n")
+                continue
+
+          
+            else:
+                print("\nEscolha uma das opções oferecidas!")
+                continue
+
+            break
+        except ValueError:
+            print("Escolha um valor válido (entre 0 e 6)!")
+main()
