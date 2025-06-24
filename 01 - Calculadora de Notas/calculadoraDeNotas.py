b009f4083d26dffa2f726e28e32affdaf584b07d
João Pedro Macena <joaomacenactba@gmail.com>
2025-06-23 23:34:59 -0300

    Update calculadoraDeNotas.py

--- 01 - Calculadora de Notas/calculadoraDeNotas.py
+++ 01 - Calculadora de Notas/calculadoraDeNotas.py
@@ -16,41 +16,43 @@ def soma_total(nota1, nota2, nota3):
 def divisao_soma(soma):
     return soma / 3
 
-def resultado_média(média):
-    if média <= 4:
+def resultado_media(media):
+    if media <= 4:
         print("Status: REPROVADO!")
         print(feedbackRuimPrint)
 
-    elif 4 < média <= 6.99:
+    elif 4 < media <= 6.99:
         print("Status: RECUPERAÇÃO!")
         print(feedbackMédioPrint)
 
-    elif 7 <= média <= 8.99:
+    elif 7 <= media <= 8.99:
         print("Status: APROVADO!")
         print(feedbackÓtimoPrint)
 
-    elif 9 <= média <= 10:
+    elif 9 <= media <= 10:
         print("Status: APROVADO!")
         print(feedbackFantáticoPrint)
 
 def main():
-    try:
-        nota1 = float(input('Insira sua primeira nota: '))
-        nota2 = float(input('Insira sua segunda nota: '))
-        nota3 = float(input('Insira sua terceira nota: '))
+    while True:
+        try:
+            nota1 = float(input('Insira sua primeira nota: '))
+            nota2 = float(input('Insira sua segunda nota: '))
+            nota3 = float(input('Insira sua terceira nota: '))
 
-        if not (0 <= nota1 <= 10 and 0 <= nota2 <= 10 and 0 <= nota3 <= 10):
-            print("As notas devem estar entre 0 e 10.")
-            return
+            if not (0 <= nota1 <= 10 and 0 <= nota2 <= 10 and 0 <= nota3 <= 10):
+                print(f"\nAs notas devem estar entre 0 e 10.")
+                continue
 
-        soma = soma_total(nota1, nota2, nota3)
-        média = divisao_soma(soma)
+            soma = soma_total(nota1, nota2, nota3)
+            media = divisao_soma(soma)
 
-        print(f"\nA soma total de suas notas é: {soma}")
-        print(f"Sua média é: {média:.2f}\n")
+            print(f"\nA soma total de suas notas é: {soma}")
+            print(f"Sua média é: {media:.2f}\n")
 
-        resultado_média(média)
+            resultado_media(media)
+            break
 
-    except ValueError:
-        print("Insira apenas números válidos.")
+        except ValueError:
+            print("Insira apenas números válidos.")
 main()
